73a2e94a3b4e53eb013e542843d2b28f75368ae2
spencerpomme <spencerpomme@live.com>
2016-04-28 18:41:46 +0800

    APL28

--- test_screwpie.py
+++ test_screwpie.py
@@ -3,4 +3,5 @@ from bs4 import BeautifulSoup
 url = "https://www.douban.com/group/kaopulove/discussion?start=0"
 html = urlopen(url)
 soup = BeautifulSoup(html, "lxml")
-table = soup.findAll()
+table = soup.findAll("table", {"class": "olt"})
+total_topic_data =  soup.find("span", {"class":"thispage"}).attrs["data-total-page"]
