960e2ab8be90cd6caf249e34ffa9a5b8635ad867
spencerpomme <spencerpomme@live.com>
2016-04-28 20:35:06 +0800

    APL28

--- test_screwpie.py
+++ test_screwpie.py
@@ -1,7 +1,21 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+
 url = "https://www.douban.com/group/kaopulove/discussion?start=0"
 html = urlopen(url)
 soup = BeautifulSoup(html, "lxml")
+
+# the main topic table
 table = soup.findAll("table", {"class": "olt"})
+# finds the total topics
 total_topic_data =  soup.find("span", {"class":"thispage"}).attrs["data-total-page"]
+# finds rows of the table, exclude <tr> tags that not holding valid topic data
+# the "id" key is to exclude javascript at the beginning of the table rows
+rows = list(table)[0].findAll("tr", {"class": "", "id": ""})
+
+
+if __name__ == "__main__":
+
+    print("---*---" * 4)
+
+    print(rows[0])
