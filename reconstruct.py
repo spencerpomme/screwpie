c03fbc565ca4efde3e8b950543a6ed04efd3e261
spencerpomme <spencerpomme@live.com>
2016-05-03 21:18:52 +0800

    MAY03

--- reconstruct.py
+++
@@ -1,9 +0,0 @@
-#! python3
-# This script reconstruct the huge startOperation function into several samll
-# functions.
-
-def startOperation(init_url:str, pages:int=None, filename:str="TSTCSV")->list:
-    """
-    This function starts core process of crawler.
-    """
-    
