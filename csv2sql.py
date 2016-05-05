5e67f8109bbfed2fe36bc22618deb9b8f2ace94a
spencerpomme <spencerpomme@live.com>
2016-05-05 20:57:30 +0800

    MAY05

--- csv2sql.py
+++ csv2sql.py
@@ -1,12 +1,17 @@
 # Test file on transfer csv file data to sql database
 import csv
+import psycopg2
+
+conn = psycopg2.connect("dbname=douban user=postgres password=123456")
+curr = conn.cursor()
+
 with open('test.csv', encoding='utf8') as csvfile:
     i = 0
     reader = csv.reader(csvfile)
     for row in reader:
         if i < 10:
-            print(row[0])
-            # print(type(row))
+            print(row[-1])
+            print(type(row[-1]))
             i += 1
         else:
             break
