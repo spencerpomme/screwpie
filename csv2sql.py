6c025e26451af2bdffb9967b3e13235e424061da
spencerpomme <spencerpomme@live.com>
2016-05-05 23:41:37 +0800

    MAY05

--- csv2sql.py
+++ csv2sql.py
@@ -3,7 +3,7 @@ import csv
 import psycopg2
 
 secret = input("Please input db password!\n")
-conn = psycopg2.connect(database="douban", user="postgres", password=secret)
+conn = psycopg2.connect(database="douban", user="zhangpingcheng", password=secret)
 curr = conn.cursor()
 
 with open('test.csv', encoding='utf8') as csvfile:
@@ -12,8 +12,11 @@ with open('test.csv', encoding='utf8') as csvfile:
     for row in reader:
         if i < 10:
             print(row)
-            curr.execute("INSERT INTO gzzf VALUES (%d, %s, %s, %s, %s, %d, %s);" % (
-                 row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+            print(len(row))
+            curr.execute("INSERT INTO gzzf VALUES (%d, '%s', '%s', '%s', '%s', %d, '%s');" % (
+                 i, row[0], row[1], row[2], row[3], int(row[4]), row[5]))
             i += 1
         else:
             break
+curr.close()
+conn.close()
