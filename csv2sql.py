aeb5692c76c0ae6958fe5a8b713a1b6a648ae53c
spencerpomme <spencerpomme@live.com>
2016-05-09 23:15:38 +0800

    MAY09

--- csv2sql.py
+++ csv2sql.py
@@ -21,34 +21,54 @@ def getdate(rawstr:str)->str:
     else:
         raise FailDateRetrive
 
-def main():
+def createTable(tbname:str, cursor):
+    """
+    This function create a new table in database.
+    tbname: the name of to be created table
+    configDict: field name as key and tuple (type, constrain) as value
+
+    """
+    cursor.execute("""CREATE TABLE %s (
+            id INTEGER PRIMARY KEY,
+            title VARCHAR(256) ,
+            title_url VARCHAR(256),
+            author VARCHAR(256),
+            author_url VARCHAR(256),
+            follow INTEGER ,
+            lastres DATE NOT NULL);
+            """ % tbname)
+
+def csv2sql(filename:str, tablename: str, cursor):
     """
     Main process, insert csv file into chosen database.
-    """
-    dtbase, usrname, secret = ('douban', 'postgres', '123456')
-    """
+
     dtbase = input("Please input db name:\n")
     usrname = input("Please input username:\n")
     secret = input("Please input db password!\n")
     """
-    conn = psycopg2.connect(database=dtbase, user=usrname, password=secret)
-    curr = conn.cursor()
-    with open('test.csv', encoding='utf8') as csvfile:
+    with open(filename, encoding='utf8') as csvfile:
         i = 0
-        reader = csv.reader(csvfile)
+        reader = csv.reader(csvfile, cursor)
+        createTable(tablename, cursor)
         for row in reader:
             #print(row)
             title, title_url, author, author_url, follow = row[0:5]
             datestring = getdate(row[5])
-            print(title, follow, datestring)
-            curr.execute("INSERT INTO kaopulove VALUES \
-                         (%d,'%s','%s','%s','%s',%d,'%s')"%(i, title, title_url,
-                          author, author_url, int(follow), datestring)
-                         )
+            # print(title, follow, datestring)
+            cursor.execute("INSERT INTO kaopulove VALUES \
+                           (%d,'%s','%s','%s','%s',%d,'%s')"%(i, title, title_url,
+                            author, author_url, int(follow), datestring)
+                           )
             i += 1
         print('done')
-    conn.commit()
-    conn.close()
 
 if __name__ == "__main__":
-    main()
+    tablename = "kaopulove"
+    dtbase, usrname, secret = ('douban', 'postgres', '123456')
+    conn = psycopg2.connect(database=dtbase, user=usrname, password=secret)
+    curr = conn.cursor()
+    curr.execute("drop table %s;" % tablename)
+    csv2sql("test.csv", tablename, curr)
+    conn.commit()
+    conn.close()
+    print("transaction finished.")
