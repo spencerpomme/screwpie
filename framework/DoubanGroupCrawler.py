28c6bca071b30ef2e04d4a2212fd1a249ff7e4f8
spencerpomme <spencerpomme@live.com>
2017-01-10 22:28:36 +0800

    remove file

--- framework/DoubanGroupCrawler.py
+++
@@ -1,9 +0,0 @@
-'''
-This is a web crawler collecting Douban Group data.
-
-This class extends the BaseCrawler class and adds website specific functions.
-'''
-
-from BaseCrawler import BaseCrawler
-
-print(dir(BaseCrawler))
