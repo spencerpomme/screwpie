fda91cd0ceae47b5711824e2c402e676ddf56807
spencerpomme <spencerpomme@live.com>
2017-04-29 16:49:19 +0800

    Tiny modification

--- dev/zhihu.py
+++ dev/zhihu.py
@@ -8,7 +8,7 @@ filename = '%s.csv' % (USERNAME)
 offset = 0
 
 headers = {
-        'Cookie': 'd_c0="ABACeiPuKwuPTi544NVJjPTaS40_R6gz5LI=|1484706668"; _zap=a062f552-499f-4c8f-a261-09689401a4bd; login="MzBjZjMyYWYwZDY4NGIxZWEwNDI2M2E4OTM3Mzg0OWM=|1486496120|06ac1db6e293eb4d089706c963a1ca6891b798cf"; _xsrf=01e512bb5c689ec22209b1e6f4e480f4; q_c1=02d871bad1b6434086fe737982df2a3d|1487334669000|1484706667000; capsion_ticket="2|1:0|10:1488118101|14:capsion_ticket|44:YTJmZmJkZjgxZDRkNDMyMjgyYmExYTRiOGI0MGViMzQ=|a2062195ba6dbec3966b6330adf194956de0530140cbbea3e7268c450e8e38a8"; cap_id="YjNjYmJmZDhjYTljNDg5OWFiZjUxODVhZjNhNzg0MzQ=|1488118104|dcdaac05d9812f828b138753c6f4d8a1ee1de423"; l_cap_id="OTc1Zjc5YTBmM2Y3NDQ0Y2E2NTJkODYwOTRmNDZhZTg=|1488118104|f6806a6483de467f42e1fa9a9499bf468ffe203d"; nweb_qa=heifetz; aliyungf_tc=AQAAABhu0l2bXAcArUUgd5Q4rHziy6ti; __utma=51854390.1439269847.1488033758.1488332742.1488336624.17; __utmb=51854390.0.10.1488336624; __utmc=51854390; __utmz=51854390.1488016023.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=51854390.100-1|2=registration_date=20131007=1^3=entry_date=20131007=1; z_c0=Mi4wQUFEQTBzY2VBQUFBRUFKNkktNHJDeGNBQUFCaEFsVk5XbTdhV0FCVEF6bDdzV2lWM3kwUW9XaFhKMG9LbDdpckRn|1488336628|6e6d547452bad801110bdc1b0695e031e7b9f722',
+        'Cookie': 'q_c1=28ada5f3722f4c5d93126dc90d73ba06|1492403582000|1492403582000; d_c0="AHDCqImfnguPTrVW0nJimSjiexf7jOf1yxg=|1492403583"; _zap=a66a75d3-06c5-4cae-a4d3-8b33ef75e9af; _xsrf=cd9131c0f32b50a1973f678628692c90; capsion_ticket="2|1:0|10:1492697361|14:capsion_ticket|44:NzdlNGM0Yjc1ZDA1NGZlYmEwMWU0ZmJkYjkwNWMxZjM=|49d838c11200d10de6d24bf67f9d67f21d9b103d32f4bda83c5472defbe5fe43"; r_cap_id="OTVmYTE2YjRhZGRlNGMzMzljZjVkODRiYWMxZTkyMGI=|1492697363|a2a80e98c76ff347e485397c438fd1c0d3035bfb"; cap_id="NjkwNTg1MTFkYjA3NGU4OWFkZGRhMDEwZWNjMWQ0MDc=|1492697363|3fcb0ff569f45a55b116ae6cf7e6babec26b4fca"; l_cap_id="OTc3MGZiNzdlZTY4NDQ1MDg3ODY0ZTQwNjQyNTRkODY=|1492697363|9b11300357019f8cf140870acefad160f65c07c7"; aliyungf_tc=AQAAANh9nT3OrwYAxkUgd/n5mB3Yi3/p; acw_tc=AQAAADhMO1UHNwkAxkUgdxuijGgi8jty; __utma=51854390.1375139231.1492952772.1493364117.1493374940.27; __utmb=51854390.0.10.1493374940; __utmc=51854390; __utmz=51854390.1493358415.25.4.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/people/Evans23/answers; __utmv=51854390.100-1|2=registration_date=20131007=1^3=entry_date=20131007=1; z_c0=Mi4wQUFEQTBzY2VBQUFBY01Lb2laLWVDeGNBQUFCaEFsVk5HMDRnV1FEaUg0WVpjTWxhczdvWHVCUzhEc2V0a2RfZmxR|1493374998|f64f34d09ace7e7a04ab12ac40d23076dbfd5bc0',
         'Host':'www.zhihu.com',
         'Referer':'http://www.zhihu.com/',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
@@ -60,5 +60,3 @@ while offset < total_follower:
     time.sleep(0.5)
 print('done')
 file.close()
-
-
